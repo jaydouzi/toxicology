080f0824c44c4ffacb68471d8d68e13648e17457
Enrique Castillo <enrique.castillox@gmail.com>
2017-02-28 18:44:10 -0800

    rewrite, install nvm, node, electron, etc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y nodejs npm
-npm install --global electron
-
+if [ ! -f ~/runonce ]
+then
+	cd /home/vagrant
+	echo "Installing nvm..."
+	wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+	export NVM_DIR="/home/vagrant/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+		nvm install node
+		npm install bower -g
+		npm install electron -g
+		touch ~/runonce
+	else
+		echo "nvm already installed"
+		nvm node --version
+fi	
