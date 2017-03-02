94a8072414cb2940e06c3dbbabe5a61ea28ead28
Enrique Castillo <enrique.castillox@gmail.com>
2017-03-01 20:35:50 -0800

    update to point to new main window

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ let win
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 400, height: 250 })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'app/chat.html'),
+        pathname: path.join(__dirname, 'app/main/index.html'),
         protocol: 'file:',
         slashes: true
     }))
