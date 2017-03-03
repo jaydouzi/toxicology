cf1633a24b5ce4e225a1d8e4e1651a207a7bf148
Enrique Castillo <enrique.castillox@gmail.com>
2017-03-02 21:43:04 -0800

    restructure

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ function createWindow() {
 
     // Load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'app/main/index.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
